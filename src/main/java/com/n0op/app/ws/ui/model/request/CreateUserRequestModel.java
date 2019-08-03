07f9069aa0d65d49d74f1b846fcaea4233178594
n0-op <daniel.marquez9087@gmail.com>
2019-08-03 11:24:41 -0700

    Adding users services

--- src/main/java/com/n0op/app/ws/ui/model/request/CreateUserRequestModel.java
+++ src/main/java/com/n0op/app/ws/ui/model/request/CreateUserRequestModel.java
@@ -1,7 +1,56 @@
 package com.n0op.app.ws.ui.model.request;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
 /**
  * @author DanM
  */
+@XmlRootElement
 public class CreateUserRequestModel {
+
+    private String userName;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
